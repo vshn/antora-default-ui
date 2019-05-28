1bcb43d08307233ae4e8be175ec9f6b3fb65cfa7
Adrian Kosmaczewski <adrian@vshn.ch>
2019-05-28 20:12:02 +0200

    Fixed gulp build

--- src/js/06-vshn-search.js
+++ src/js/06-vshn-search.js
@@ -1,83 +1,83 @@
 ; (function () {
-    'use strict'
+  'use strict'
 
-    function find(selector, from) {
-        return [].slice.call((from || document).querySelectorAll(selector))
+  function find (selector, from) {
+    return [].slice.call((from || document).querySelectorAll(selector))
+  }
+
+  function removeAllChildren (div) {
+    while (div.firstChild) {
+      div.removeChild(div.firstChild)
     }
+  }
 
-    function removeAllChildren(div) {
-        while (div.firstChild) {
-            div.removeChild(div.firstChild);
-        }
-    }
+  var allMain = find('.main')
+  var allDocs = find('.doc')
 
-    var allMain = find('.main')
-    var allDocs = find('.doc')
+  if (allMain.length !== 1 || allDocs.length !== 1) {
+    console.error('Not found required elements in page')
+    return
+  }
 
-    if (allMain.length !== 1 || allDocs.length !== 1) {
-        console.error('Not found required elements in page')
-        return
-    }
+  var main = allMain[0]
+  var mainDoc = allDocs[0]
 
-    var main = allMain[0]
-    var mainDoc = allDocs[0]
-
-    var documents = [{
-        "name": "Lunr",
-        "text": "Like Solr, but much smaller, and not as bright."
-    }, {
-        "name": "React",
-        "text": "A JavaScript library for building user interfaces."
-    }, {
-        "name": "Lodash",
-        "text": "A modern JavaScript utility library delivering modularity, performance & extras."
-    }]
-    var lunrIndex = lunr(function () {
+  var documents = [{
+    'name': 'Lunr',
+    'text': 'Like Solr, but much smaller, and not as bright.',
+  }, {
+    'name': 'React',
+    'text': 'A JavaScript library for building user interfaces.',
+  }, {
+    'name': 'Lodash',
+    'text': 'A modern JavaScript utility library delivering modularity, performance & extras.',
+  }]
+  var lunrIndex = window['lunr'](function () {
     this.ref('name')
     this.field('text')
     this.metadataWhitelist = ['name']
-    
+
     documents.forEach(function (d) {
-        this.add(d)
+      this.add(d)
     }, this)
-    })
-      
-    var searchArticle = document.createElement('article')
-    searchArticle.className = 'doc'
-    var searchTitle = document.createElement('h1')
-    searchTitle.innerText = 'Search Results'
+  })
+
+  var searchArticle = document.createElement('article')
+  searchArticle.className = 'doc'
+  var searchTitle = document.createElement('h1')
+  searchTitle.innerText = 'Search Results'
+  searchArticle.appendChild(searchTitle)
+
+  function displayResults (results, div) {
+    removeAllChildren(div)
     searchArticle.appendChild(searchTitle)
-
-    function displayResults(results, div) {
-        removeAllChildren(div)
-        searchArticle.appendChild(searchTitle)
-        results.forEach(function (item, idx) {
-            var searchResult = document.createElement('p')
-            searchResult.innerText = item.ref
-            div.appendChild(searchResult)
-        })
-    }
-
-    find('#search-input').forEach(function (item, idx) {
-        item.onkeyup = function() {
-            var val = item.value
-            if (val.length > 0) {
-                if (!searchArticle.parentNode) {
-                    main.replaceChild(searchArticle, mainDoc)
-                }
-                var results = lunrIndex.search(val)
-                if (results.length > 0) {
-                    console.info('Found %s results', results.length)
-                    displayResults(results, searchArticle)
-                } else {
-                    console.warn('No search results found')
-                    displayResults([{ref: 'No results found'}], searchArticle)
-                }
-            } else {
-                if (!mainDoc.parentNode) {
-                    main.replaceChild(mainDoc, searchArticle)
-                }
-            }
-        }
+    results.forEach(function (item, idx) {
+      var searchResult = document.createElement('p')
+      searchResult.innerText = item.ref
+      div.appendChild(searchResult)
     })
+  }
+
+  find('#search-input').forEach(function (item, idx) {
+    item.onkeyup = function () {
+      var val = item.value
+      if (val.length > 0) {
+        if (!searchArticle.parentNode) {
+          main.replaceChild(searchArticle, mainDoc)
+        }
+        var results = lunrIndex.search(val)
+        if (results.length > 0) {
+          console.info('Found %s results', results.length)
+          displayResults(results, searchArticle)
+        } else {
+          console.warn('No search results found')
+          displayResults([{ ref: 'No results found' }], searchArticle)
+        }
+      } else {
+        if (!mainDoc.parentNode) {
+          main.replaceChild(mainDoc, searchArticle)
+        }
+      }
+    }
+  })
 })()
