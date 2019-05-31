12f89853e0fd3f87ef9a92793fb2504c2a0ea8a2
Adrian Kosmaczewski <adrian@vshn.ch>
2019-05-31 16:27:08 +0200

    Much nicer search results

--- src/js/06-vshn-search.js
+++ src/js/06-vshn-search.js
@@ -42,17 +42,28 @@
     searchParagraph.appendChild(searchExcerpt)
 
     var searchDiv = document.createElement('div')
-    searchDiv.className = 'paragraph'
+    searchDiv.className = 'search-div paragraph'
+    searchDiv.onclick = function () {
+      window.location.href = item.href
+    }
     searchDiv.appendChild(searchParagraph)
     return searchDiv
   }
 
   // Builds the list of search results on the node passed as parameter.
   // The results variable is an array of objects with 'name' and 'href' keys.
-  function displayResults (results, query) {
+  function display (results, query) {
+    if (isEmptyOrBlank(query)) {
+      // Display the original page in lieu of the search results if not done yet
+      if (!mainDoc.parentNode) {
+        main.replaceChild(mainDoc, searchArticle)
+      }
+      return
+    }
+    // Rebuild the contents of the "search results" page
     removeAllChildren(searchArticle)
     searchArticle.appendChild(searchTitle)
-    searchTitle.innerText = 'Search Results for "' + query + '"'
+    searchTitle.innerText = 'Search Results for "' + query.trim() + '"'
     if (results.length === 0) {
       var searchResult = document.createElement('p')
       searchResult.innerText = 'No results found.'
@@ -63,35 +74,32 @@
         searchArticle.appendChild(searchDiv)
       })
     }
+    // Replace the current page with a "search results" page if not done yet
+    if (!searchArticle.parentNode) {
+      main.replaceChild(searchArticle, mainDoc)
+    }
   }
 
   // Performs the actual search and drives the display of results
-  function searchAndDisplay (query) {
-    if (!isEmptyOrBlank(query)) {
-      query = query.trim()
-      // Replace the current page with a "search results" page
-      if (!searchArticle.parentNode) {
-        main.replaceChild(searchArticle, mainDoc)
-      }
-      // Search and look for the corresponding files, but return at most 10 items
-      var results = lunrIndex.search(query).slice(0, 10).map(function (result) {
-        return origin[result.ref]
-      })
-      displayResults(results, query)
-    } else {
-      // Display the original page in lieu of the search results
-      if (!mainDoc.parentNode) {
-        main.replaceChild(mainDoc, searchArticle)
-      }
+  function search (query) {
+    if (isEmptyOrBlank(query)) {
+      return []
     }
+    // Search and look for the corresponding files, but return at most 10 items
+    var results = lunrIndex.search(query.trim()).slice(0, 10).map(function (result) {
+      return origin[result.ref]
+    })
+    return results
   }
 
   var main = document.querySelector('.main')
   var mainDoc = document.querySelector('.doc')
+  var searchInput = document.querySelector('#search-input')
 
   // Just to make sure that there is a place where to show search results
-  if (!main || !mainDoc) {
-    console.error('Not found required elements in page with CSS classes "main" and "doc".')
+  if (!main || !mainDoc || !searchInput) {
+    console.error('Not found required elements in page with CSS classes "main" and "doc",')
+    console.error('or a text field with ID "search-input".')
     return
   }
 
@@ -114,17 +122,18 @@
   var searchTitle = document.createElement('h1')
   searchArticle.appendChild(searchTitle)
 
-  // Find a reference to the input box used to enter search terms
-  var searchInput = document.querySelector('#search-input')
-
   // Event to be fired everytime the user presses a key
   searchInput.onkeyup = function () {
-    searchAndDisplay(searchInput.value)
+    var query = searchInput.value
+    var results = search(query)
+    display(results, query)
   }
 
   // Event to be fired when the input gains focus
   searchInput.onfocus = function () {
-    searchAndDisplay(searchInput.value)
+    var query = searchInput.value
+    var results = search(query)
+    display(results, query)
   }
 
   // Focus the search box when the page loads
