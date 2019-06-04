fefb0b3e222f3944c6ce9339ced4ff1fb554fca1
Adrian Kosmaczewski <adrian@vshn.ch>
2019-06-04 15:19:51 +0200

    Better handling of the redirection to GitLab

--- src/js/05-vshn-edit-url.js
+++ src/js/05-vshn-edit-url.js
@@ -8,23 +8,25 @@
   // toolbar.hbs) we "rewrite" the URL passed (which is local to the
   // context used to create the site, usually with "file://" URLs) and we
   // redirect to the actual repository where the pages are stored.
-  function vshnEditUrl (originalPath) {
-    // The originalPath variable has a string with the following form:
-    // 'file:///home/user/path/handbook/modules/ROOT/pages/squads.adoc'
-    // We match "*/modules/ROOT/pages/*" and extract the middle section.
-    var re = new RegExp('^(.*)modules/ROOT/pages/(.*)$')
+  function vshnEditUrl () {
+    var originalPath = window.location.pathname
+    // The window.location.pathname property is a string with the following form:
+    // '/handbook/1.0.0/your_first_day.html'
+    // We match and extract the middle section.
+    var re = new RegExp('^/handbook/(.*)/(.*).html$')
 
     /*
     At this point, "matches" is an array with the following structure:
-    0: "file:///home/user/path/handbook/modules/ROOT/pages/squads.adoc"
-    1: "file:///home/user/path/handbook/"
-    2: "squads.adoc"
+    0: "/handbook/1.0.0/your_first_day.html"
+    1: "1.0.0"
+    2: "your_first_day"
     */
     var matches = originalPath.match(re)
+    console.log(matches)
     if (matches.length > 2) {
       // Create the actual URL that points to the source of this file
-      var somewhere = matches[0].replace(matches[1], '')
-      var newPath = 'https://git.vshn.net/vshn/handbook/tree/develop/' + somewhere
+      var newPath = 'https://git.vshn.net/vshn/handbook/tree/develop/modules/ROOT/pages/' + matches[2] + '.adoc'
+      console.log(newPath)
       window.location = newPath
     } else {
       console.error('Could not redirect the user properly using URL:' + originalPath)
@@ -35,8 +37,7 @@
   var editButton = document.querySelector('.vshn-page-edit')
 
   // Add an onclick event handler for the corresponding button on the UI
-  var path = editButton.dataset.url
   editButton.onclick = function () {
-    vshnEditUrl(path)
+    vshnEditUrl()
   }
 })()
